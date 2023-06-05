fe6d87c6e27d10bd7fffb38fd0743a67858e5d63
Nam Chan <namanhseoul@gmail.com>
2023-06-05 21:21:13 +0700

    Demo Insertion Sort

---
+++ Demo Insertion Sort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int data[5] = {23,90,9,25,16};
+	int temp, i, j;
+	// perform insertion sorting
+	for(j = 1; j < 5; j++){
+		i = j - 1;
+		temp = data[j];
+		while((i >= 0) && (temp < data[i])){
+			data[i + 1] == data[i];
+			i--;
+		}
+		data[i + 1] = temp;
+	}
+	for(i = 0; i < 5; i++){
+		printf("\n%d", data[i]);
+	}
+	return 0;
+}
