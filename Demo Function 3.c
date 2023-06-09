245a2a98989c414bf2eaee364b9d3babc68f18b8
Nam Chan <namanhseoul@gmail.com>
2023-06-10 00:14:21 +0700

    Update Demo Function 3.c

--- Demo Function 3.c
+++ Demo Function 3.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+// Code function change Value
 void change(int num){
 	num = num + 200;
 	printf("\nGia tri truyen vao: %d", num);
@@ -6,6 +8,7 @@ void change(int num){
 
 int main(){
 	int x = 10;
+	// Call function
 	change(x);
 	printf("\nGia tri x sau khi goi ham: %d", x);
 	return 0;
