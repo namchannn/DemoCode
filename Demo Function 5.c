4376b85db05b4fb8077c73f66c2e25755d4385a1
Nam Chan <namanhseoul@gmail.com>
2023-06-09 19:34:17 +0700

    Create Demo Function 5.c

---
+++ Demo Function 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main(){
+	int i = 5, j = 10;
+	swap(&i, &j); // Goi va truyen tham chieu aka call by reference
+	printf("\nGia tri cua i: %d va gia tri cua j: %d", i, j);
+	return 0;
+}
