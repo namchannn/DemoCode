7d9a19db3a921e76b1259e84b46439796c95de31
Nam Chan <namanhseoul@gmail.com>
2023-06-10 01:46:09 +0700

    Update Demo Function 1.c

--- Demo Function 1.c
+++ Demo Function 1.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-// Khai bao ham ten: add voi 2 tham so x, y
+// Khai báo hàm tên: add với 2 tham số x, y
 
 // Step 1
 int add(int x, int y);
 
 int main(){
 	int a = 5, b = 10, total = 0;
-	// Goi ham (yeu cau ham add thuc thi)
-	// Step 2 - Goi ham
+	// Gọi hàm (yều cầu hàm add thực thi)
+	// Step 2 - Gọi hàm
 	total = add(a, b); // => x = a; y = b;
 	printf("%d", total);
 }
 
 // Step 3 - Coding
-// Trien khai code trong ham add
+// Triển khai code trong hàm add
 int add(int x, int y){
 	int sum;
 	sum = x + y;
-	// Ket qua tinh toan cua ham se tra ve cho noi goi ham add
+	// Kết quả tính toán của hàm sẽ trả về chỗ nơi gọi hàm add
 	return sum;
 }
