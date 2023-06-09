bfba133a4ec7478e9c4b6e69bb7446364cef6b8e
Nam Chan <namanhseoul@gmail.com>
2023-06-09 19:17:44 +0700

    Create Demo Function 3.c

---
+++ Demo Function 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void change(int num){
+	num = num + 200;
+	printf("\nGia tri truyen vao: %d", num);
+}
+
+int main(){
+	int x = 10;
+	change(x);
+	printf("\nGia tri x sau khi goi ham: %d", x);
+	return 0;
+}
