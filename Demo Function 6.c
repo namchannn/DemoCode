ddfacb7891821f61337b78c5f19d2809f15d959c
Nam Chan <namanhseoul@gmail.com>
2023-06-10 00:14:27 +0700

    Update Demo Function 6.c

--- Demo Function 6.c
+++ Demo Function 6.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
+// Code function change Value
 void swap(int a, int b){
 	int temp = a;
 	a = b;
 	b = temp;
 	printf("\nGia tri cua a: %d va gia tri cua b: %d", a, b);	
 }
+
 int main(){
 	int i = 5, j = 10;
 	swap(i, j); // Goi va truyen tham chieu aka call by value
