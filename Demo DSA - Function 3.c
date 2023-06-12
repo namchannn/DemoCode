58069680f975c0264b2b198cb3e53a8462add041
Nam Chan <namanhseoul@gmail.com>
2023-06-13 01:04:28 +0700

    Update Demo DSA - Function 3.c

--- Demo DSA - Function 3.c
+++ Demo DSA - Function 3.c
@@ -4,19 +4,17 @@
 // Merge function
 void merge(int arr[], int l, int m, int r){
 	int i, j, k;
-	int n1 = m - 1 + 1;
+	int n1 = m - l + 1;
 	int n2 = r - m;
 	int L[n1], R[n2];
 	for(int i = 0; i < n1; i++){
-		L[i] = arr[m + 1 + i];
+		L[i] = arr[l + i];
 		for(int j = 0; j < n2; j++){
 			R[j] = arr[m + 1 + j];
 			i = 0;
 			j = 0;
 			k = l;
 		}
-	}
-	while(i < n1 && j < n2){
 		if(L[i] <= R[j]){
 			arr[k] = L[i];
 			i++;
@@ -24,6 +22,11 @@ void merge(int arr[], int l, int m, int r){
 			arr[k] = R[j];
 			j++;
 		}
+		k++;
+	}
+	while(i < n1){
+		arr[k] = L[i];
+		i++;
 		k++;
 	}
 	while(j < n2){
@@ -36,7 +39,7 @@ void merge(int arr[], int l, int m, int r){
 // Merge Sort function
 void mergeSort(int arr[], int l, int r){
 	if(l < r){
-		int m = l + (r - 1) / 2;
+		int m = l + (r - l) / 2;
 		mergeSort(arr, l, m);
 		mergeSort(arr, m + 1, r);
 		merge(arr, l, m, r);
